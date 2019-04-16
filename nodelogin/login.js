bcf3a18ed0d26d794781a50e41e6b025c0c9408d
franzkroll <franz.kroll@b-tu.de>
2019-04-16 17:15:04 +0200

    server update

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -7,7 +7,7 @@ var path = require('path');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'franz',
-    password : '',
+    password : 'shera123!',
     database : 'nodelogin'
 });
 
@@ -43,6 +43,35 @@ app.post('/auth', function(request, response) {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
 });
 
 app.listen(3000);
