ca7c18ba94c66434b53fe6156fbedc2a6c646e85
franzkroll <franz.kroll@b-tu.de>
2019-04-25 16:26:18 +0200

    working chart example

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -1,10 +1,10 @@
-var mysql = require('mysql');
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var http = require('http');
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
 
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -12,6 +12,8 @@ var connection = mysql.createConnection({
     password : 'test',
     database : 'nodelogin'
 });
+
+//TODO create data connection
 
 var app = express();
 
@@ -48,30 +50,19 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
 
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
-		//response.send('Welcome back, ' + request.session.username + '!');
 		response.setHeader('Content-Type', 'text/html');
+		/*connection.query("SELECT * FROM accounts",(err, result)=>{
+			if (err) {
+				console.log(err); 
+				response.json({"error":true});
+			} else { 
+				console.log(result); 
+				response.json(result);
+			}
+		});*/
 		response.sendFile(path.join(__dirname+'/chart.html'));
 	} else {
 		response.send('Please login to view this page!');
