aaf45da275525201e63e11d90019d880b87576ff
franzkroll <franz.kroll@b-tu.de>
2019-04-29 14:17:26 +0200

    cleanup

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.render('pages/login');
 });
 
 app.post('/auth', function(request, response) {
