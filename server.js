8e82d12bcf750aaff5c70becdb8f977a7bd6438f
franzkroll <franz.kroll@b-tu.de>
2019-05-07 17:32:55 +0200

    cleanup, better folder structure

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const serverStatus = require('express-server-status');
+const helmet = require('helmet');
+
+const port = 4000;
+
+var routes = require('./routes/index.js');
+
+// Application uses express for rendering and displaying pages
+var app = express();
+
+// Set ejs as view engine
+app.set('view engine', 'ejs');
+
+app.use(helmet());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(bodyParser.json());
+
+// Display simple server status
+app.use('/stats', serverStatus(app));
+
+routes(app);
+
+// Start server on port that was previously defined
+app.listen(port, function () {
+	console.log('Server listening on port ' + port + ' …');
+});
