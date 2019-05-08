d73d908be9cbdbd09fa6254ac7263236ccc54f1d
franzkroll <franz.kroll@b-tu.de>
2019-05-08 12:02:30 +0200

    moved scripts in templates to /public/scripts

---
+++ public/scripts/graph.js
@@ -0,0 +1,71 @@
+var selectAction = document.getElementById("graph");
+var currentChart;
+var chart = document.getElementById('chart').getContext('2d');
+
+selectAction.onchange = function (e) {
+    if (!e) {
+        var e = window.event;
+    }
+
+    var svalue = this.options[this.selectedIndex].value;
+    if (currentChart) {
+        currentChart.destroy();
+    }
+
+    if (svalue === "line") {
+        currentChart = new Chart(chart).Line(buyerData);
+    } else if (svalue === "pie") {
+        currentChart = new Chart(chart).Pie(pieData, pieOptions);
+    } else if (svalue === "bar") {
+        currentChart = new Chart(chart).Bar(barData);
+    }
+}
+
+var buyerData = {
+    labels: ["Januar", "Februar", "März", "April", "Mai", "Juni"],
+    datasets: [{
+        fillColor: "rgba(172,194,132,0.4)",
+        strokeColor: "#ACC26D",
+        pointColor: "#fff",
+        pointStrokeColor: "#9DB86D",
+        data: [203, 156, 99, 251, 305, 247]
+    }]
+}
+
+var pieData = [{
+        value: 20,
+        color: "#878BB6"
+    },
+    {
+        value: 40,
+        color: "#4ACAB4"
+    },
+    {
+        value: 10,
+        color: "#FF8153"
+    },
+    {
+        value: 30,
+        color: "#FFEA88"
+    }
+];
+
+var pieOptions = {
+    segmentShowStroke: false,
+    animateScale: true
+}
+
+var barData = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [{
+            fillColor: "#48A497",
+            strokeColor: "#48A4D1",
+            data: [456, 479, 324, 569, 702, 600]
+        },
+        {
+            fillColor: "rgba(73,188,170,0.4)",
+            strokeColor: "rgba(72,174,209,0.4)",
+            data: [364, 504, 605, 400, 345, 320]
+        }
+    ]
+}
