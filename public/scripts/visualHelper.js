d669e4212e093f7db399a0b898fec1b79c710cad
franzkroll <franz.kroll@b-tu.de>
2019-05-10 14:47:58 +0200

    added table example

--- public/scripts/visualHelper.js
+++ public/scripts/visualHelper.js
@@ -69,3 +69,12 @@ let barData = {
         }
     ]
 }
+
+function showTable() {
+    var x = document.getElementById("table");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
